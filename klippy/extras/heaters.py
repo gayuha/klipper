abae5e8f700c554c50ef60c050ccd7d6eee39b02
Guy Shapira <gayuha@gmail.com>
2020-09-21 20:33:20 +0300

    heaters.py: Added PID temperature scaling
    Added additional term to the heating power,
    proportional to the extruder speed.
    
    Signed-off-by: Guy Shapira <gayuha@gmail.com>

--- klippy/extras/heaters.py
+++ klippy/extras/heaters.py
@@ -84,7 +84,10 @@ class Heater:
             time_diff = read_time - self.last_temp_time
             self.last_temp = temp
             self.last_temp_time = read_time
-            self.control.temperature_update(read_time, temp, self.target_temp)
+            extruder_velocity = \
+                self.printer.lookup_object("extruder").get_velocity(read_time)
+            self.control.temperature_update(read_time, temp, self.target_temp,
+                extruder_velocity)
             temp_diff = temp - self.smoothed_temp
             adj_time = min(time_diff * self.inv_smooth_time, 1.)
             self.smoothed_temp += temp_diff * adj_time
@@ -153,7 +156,8 @@ class ControlBangBang:
         self.heater_max_power = heater.get_max_power()
         self.max_delta = config.getfloat('max_delta', 2.0, above=0.)
         self.heating = False
-    def temperature_update(self, read_time, temp, target_temp):
+    def temperature_update(self, read_time, temp, target_temp,
+        extruder_velocity):
         if self.heating and temp >= target_temp+self.max_delta:
             self.heating = False
         elif not self.heating and temp <= target_temp-self.max_delta:
@@ -182,6 +186,7 @@ class ControlPID:
         self.Kd = config.getfloat('pid_Kd') / PID_PARAM_BASE
         dead_time = config.getfloat('pid_dead_time', default=0., minval=0.)
         lookback = config.getfloat('pid_lookback', default=1, above=0.)
+        self.Kc = config.getfloat('pid_Kc', default = 0.) / PID_PARAM_BASE
         self.min_deriv_time = heater.get_smooth_time()
         imax = config.getfloat('pid_integral_max', self.heater_max_power,
                                minval=0.)
@@ -191,7 +196,8 @@ class ControlPID:
         self.prev_temp_deriv = 0.
         self.prev_temp_integ = 0.
         self.temp_history = TempHistory(dead_time, lookback)
-    def temperature_update(self, read_time, temp_real, target_temp):
+    def temperature_update(self, read_time, temp_real, target_temp,
+        extruder_velocity):
         self.temp_history.add_temp(read_time, temp_real)
         # logging.info("read temp: %f", temp_real)
         temp = self.temp_history.get_interpolated_temp()
@@ -209,11 +215,12 @@ class ControlPID:
         temp_integ = self.prev_temp_integ + temp_err * time_diff
         temp_integ = max(0., min(self.temp_integ_max, temp_integ))
         # Calculate output
-        co = self.Kp*temp_err + self.Ki*temp_integ - self.Kd*temp_deriv
-        logging.info("pid: %f@%.3f -> target=%f temp_diff=%f time_diff=%f deriv=%f err=%f integ=%f co=%f",
-           temp, read_time, target_temp, temp_diff, time_diff, temp_deriv, temp_err, temp_integ, co)
-        logging.info("prev_temp_deriv=%f min_deriv_time=%f",
-           self.prev_temp_deriv, self.min_deriv_time)
+        co = self.Kp*temp_err + \
+            self.Ki*temp_integ - self.Kd*temp_deriv + self.Kc*extruder_velocity
+        logging.info("pid: %f@%.3f -> target=%f temp_diff=%f time_diff=%f \
+            deriv=%f err=%f integ=%f co=%f extruder_velocity=%f",
+            temp, read_time, target_temp, temp_diff, time_diff, temp_deriv,
+            temp_err, temp_integ, extruder_velocity, co)
         bounded_co = max(0., min(self.heater_max_power, co))
         self.heater.set_pwm(read_time, bounded_co)
         # Store state for next measurement
